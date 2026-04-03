5bce8ae9405df9e2e8721513ce802d1c08c0465a
pekarig <pekarig@gmail.com>
2026-04-03 14:35:23 +0100

    Favicon linkek kibővítése - png és apple-touch-icon hozzáadása

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,8 @@
 
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
